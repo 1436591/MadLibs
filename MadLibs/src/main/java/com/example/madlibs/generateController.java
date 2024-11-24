47b99c232a902321bc0348ba89861ff1eb401954
1436591 <braydenoathout@gmail.com>
2024-11-23 20:04:17 -0500

    Add files via upload

--- MadLibs/src/main/java/com/example/madlibs/generateController.java
+++ MadLibs/src/main/java/com/example/madlibs/generateController.java
@@ -1,18 +1,25 @@
 package com.example.madlibs;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
+import javafx.stage.Stage;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.util.*;
 
 public class generateController {
+
+    @FXML
+    private Button replayButton;
 
     @FXML
     private TextField enterWords;
@@ -28,6 +35,16 @@ public class generateController {
 
     @FXML
     private TitledPane titlePane;
+
+    @FXML
+    void toReRun(ActionEvent event) throws IOException {
+        Parent selectLibParent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/example/madlibs/welcome.fxml")));
+        Scene selectLibScene = new Scene(selectLibParent);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(selectLibScene);
+        window.show();
+    }
 
     private final Map<String, MadLib> holidayClassMap;
     private MadLib currentMadLib;
@@ -61,6 +78,7 @@ public class generateController {
                 String finalStory = currentMadLib.generateMadLib(userInputs);
                 libText.setText(finalStory);
                 nextWordButton.setDisable(true);
+                replayButton.setDisable(false);
             }
         }
     }
@@ -88,5 +106,6 @@ public class generateController {
         userInputs.clear();
         listWords.getItems().add(prompts.get(currentPromptIndex));
         nextWordButton.setDisable(false);
+        replayButton.setDisable(true);
     }
 }
